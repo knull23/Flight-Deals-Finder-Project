2b5489846a427bf6cbcc4f425a64a6a6b7df585d
Saumil Upadhyay <111678133+knull23@users.noreply.github.com>
2024-07-21 12:45:57 +0530

    Update main.py

--- main.py
+++ main.py
@@ -29,7 +29,7 @@ data_manager.update_destination_data()
 
 customer_data = data_manager.get_customer_emails()
 # Verify the name of your email column in your sheet. Yours may be different from mine
-customer_email_list = [row["What is your email ?"] for row in customer_data]
+customer_email_list = [row["whatIsYourEmail?"] for row in customer_data]
 # print(f"Your email list includes {customer_email_list}")
 
 # ==================== Search for Flights ====================
